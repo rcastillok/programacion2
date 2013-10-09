5470e3cb6b13acb073ae0ca281916679a376ca35
rcastillok <rcastillok@gmail.com>
2013-10-09 16:42:24 -0600

    Programas día dos
    estructuras basicas

---
+++ programa1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   int n1,n2,suma;
+    cout<< "ingresar el numero #1...>" ;
+    cin>> n1;
+
+    cout<< "ingresar el numero #2...>" ;
+    cin>> n2;
+
+    suma= n1 + n2;
+
+    cout<<"La suma de los numeros es ....:"<<suma<<"\n";
+
+
+
+
+    return 0;
+}
